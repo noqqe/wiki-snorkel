13dc23048817e106db4942c1229eec8f6bed0996
Jürgen <esisteinkrauss@googlemail.com>
2020-10-21 09:54:13 +0200

    Parse wiki dumps and extracting articles

---
+++ wikiparser.py
@@ -0,0 +1,33 @@
+import xml.sax
+
+class WikiXmlHandler(xml.sax.handler.ContentHandler):
+    """Content handler for Wiki XML data using SAX"""
+    """It successfully extracts wiki articles in a Wiki dump"""
+    """ "Borrowed" from here: https://towardsdatascience.com/wikipedia-data-science-working-with-the-worlds-largest-encyclopedia-c08efbac5f5c """
+    """ using wiki dumps from here: https://dumps.wikimedia.org/ """
+    """ Files to look out for: dewiki-latest-pages-articles.xml.bz2, dewiki-latest-pages-articles1.xml.bz2, dewiki-latest-pages-articles[0-9]+.xml.bz2"""
+    def __init__(self):
+        xml.sax.handler.ContentHandler.__init__(self)
+        self._buffer = None
+        self._values = {}
+        self._current_tag = None
+        self._pages = []
+
+    def characters(self, content):
+        """Characters between opening and closing tags"""
+        if self._current_tag:
+            self._buffer.append(content)
+
+    def startElement(self, name, attrs):
+        """Opening tag of element"""
+        if name in ('title', 'text'):
+            self._current_tag = name
+            self._buffer = []
+
+    def endElement(self, name):
+        """Closing tag of element"""
+        if name == self._current_tag:
+            self._values[name] = ' '.join(self._buffer)
+
+        if name == 'page':
+            self._pages.append((self._values['title'], self._values['text']))
--- wikisnorkel.py
+++ wikisnorkel.py
@@ -1,7 +1,50 @@
-import sys
-from zimscan import Reader
+import sys, os, subprocess, mwparserfromhell
+from pprint import pprint
 
-with Reader(open(sys.argv[1], 'rb')) as reader:
-    for record in reader:
-        data = record.read()
-        print(data)
+from zimscan import Reader
+import wikiparser
+import xml.sax
+
+if len(sys.argv) == 1 or not os.path.isfile(sys.argv[1]):
+	data_path = '/Volumes/temp/wikidump/sourcefiles/dewiki-latest-pages-articles.xml.bz2' # just for my own dev purposes
+else:
+	data_path = sys.argv[1]
+
+
+
+# Object for handling xml
+handler = wikiparser.WikiXmlHandler()
+# Parsing object
+parser = xml.sax.make_parser()
+parser.setContentHandler(handler)
+# Iteratively process file
+for line in subprocess.Popen(['bzcat'], 
+                              stdin = open(data_path), 
+                              stdout = subprocess.PIPE).stdout:
+    parser.feed(line)
+    
+    # Stop when 3 articles have been found
+    if len(handler._pages) > 2:
+        break
+
+pprint(handler._pages[0])
+
+
+
+wiki = mwparserfromhell.parse(handler._pages[0][1])
+# Find the wikilinks
+wikilinks = [x.title for x in wiki.filter_wikilinks()]
+pprint(wiki.strip_code().strip())
+
+
+# with Reader(open(sys.argv[1], 'rb')) as reader:
+#     for record in reader:
+#         data = record.read()
+#         pprint(data)
+#         i = i + 1
+#         if i == 15:
+# 	        exit(0)
+
+
+
+
